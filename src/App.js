216bea7a2a193da890e0c5167e58e3eb19fd46b2
Stefano Rondelli <rstefanofil@hotmail.com>
2022-08-21 19:01:42 +0200

    Add cards display framework and state array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Tarots } from './components/Tarots'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [touchedCards, setTouchedCards] = useState([])
+  let allCards = Tarots.cards.map((card) => {
+    return card.id
+  })
+
+
+  function touchCard(event) {
+    setTouchedCards(array => [...array, +event.target.dataset.id])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(touchedCards)}
+      {console.log(allCards)}
+      {Tarots.cards.map((card, index) => {
+        return <img key={index} src={card.image} data-id={card.id} alt="tarot card" onClick={touchCard}></img>
+      })}
     </div>
   );
 }
---
+++ src/components/Tarots.js
@@ -0,0 +1,96 @@
+import t0 from '../assets/images/0.jpg'
+import t1 from '../assets/images/1.jpg'
+import t2 from '../assets/images/2.jpg'
+import t3 from '../assets/images/3.jpg'
+import t4 from '../assets/images/4.jpg'
+import t5 from '../assets/images/5.jpg'
+import t6 from '../assets/images/6.jpg'
+import t7 from '../assets/images/7.jpg'
+import t8 from '../assets/images/8.jpg'
+import t9 from '../assets/images/9.jpg'
+import t10 from '../assets/images/10.jpg'
+import t11 from '../assets/images/11.jpg'
+import t12 from '../assets/images/12.jpg'
+import t13 from '../assets/images/13.jpg'
+import t14 from '../assets/images/14.jpg'
+import t15 from '../assets/images/15.jpg'
+import t16 from '../assets/images/16.jpg'
+import t17 from '../assets/images/17.jpg'
+import t18 from '../assets/images/18.jpg'
+import t19 from '../assets/images/19.jpg'
+import t20 from '../assets/images/20.jpg'
+import t21 from '../assets/images/21.jpg'
+
+const Tarots = (() => {
+    const cards = [{ id: 0,
+        image: t0
+    },
+    { id: 1,
+        image: t1
+    },
+    { id: 2,
+        image: t2
+    },
+    { id: 3,
+        image: t3
+    },
+    { id: 4,
+        image: t4
+    },
+    { id: 5,
+        image: t5
+    },
+    { id: 6,
+        image: t6
+    },
+    { id: 7,
+        image: t7
+    },
+    { id: 8,
+        image: t8
+    },
+    { id: 9,
+        image: t9
+    },
+    { id: 10,
+        image: t10
+    },
+    { id: 11,
+        image: t11
+    },
+    { id: 12,
+        image: t12
+    },
+    { id: 13,
+        image: t13
+    },
+    { id: 14,
+        image: t14
+    },
+    { id: 15,
+        image: t15
+    },
+    { id: 16,
+        image: t16
+    },
+    { id: 17,
+        image: t17
+    },
+    { id: 18,
+        image: t18
+    },
+    { id: 19,
+        image: t19
+    },
+    { id: 20,
+        image: t20
+    },
+    { id: 21,
+        image: t21
+    }]
+
+
+    return { cards }
+})()
+
+export { Tarots }
