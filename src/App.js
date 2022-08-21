5c5b1e856d26a74beca998dea5f2e43a6bc037d8
Stefano Rondelli <rstefanofil@hotmail.com>
2022-08-22 00:16:29 +0200

    Finish game logic

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import { Display } from './components/Display'
 
 function App() {
   const [touchedCards, setTouchedCards] = useState([])
+  const [currentCards, setCurrentCards] = useState([])
   const [currentScore, setCurrentScore] = useState(0)
   const [highScore, setHighScore] = useState(0)
   const allCards = Tarots.cards.map((card) => {
     return card.id
   })
+
+  useEffect(() => {
+    setDisplay()
+  }, [])
 
   useEffect(() => {
     setCurrentScore(touchedCards.length)
@@ -23,6 +28,7 @@ function App() {
     } else {
       addCard(event)
     }
+    setDisplay()
   }
 
   function addCard(event) {
@@ -40,14 +46,52 @@ function App() {
     setHighScore(touchedCards.length)
   }
 
+  function setDisplay() {
+    let cards = prepareDisplay()
+    shuffleCards(cards)
+    cards = convertCards(cards)
+    setCurrentCards(cards)
+  }
+
+  function prepareDisplay() {
+    let eligibleCards = allCards.filter((card) => !touchedCards.includes(card))
+    let pityCard = eligibleCards[Math.floor(Math.random() * (eligibleCards.length - 1))]
+    let displayCards = [pityCard]
+    for (let i = 0; i < 9; i++) {
+      let card = displayCards[0]
+      while (displayCards.includes(card) === true || card === undefined) {
+        card = allCards[Math.floor(Math.random() * (eligibleCards.length - 1))]
+
+      }
+      displayCards.push(card)
+    }
+    return displayCards
+  }
+
+  function shuffleCards(cards) {
+    for (let i = cards.length - 1; i > 0; i--) {
+      let n = Math.floor(Math.random() * (i + 1));
+      [cards[i], cards[n]] = [cards[n], cards[i]];
+    }
+    if (cards.includes(undefined)) {
+      console.log(cards)
+    }
+  }
+
+  function convertCards(cards) {
+    cards = cards.map((id) => {
+      return Tarots.cards[id]
+    })
+    return cards
+  }
+
   return (
     <div className="App">
       <div className="flex justify-between p-15">
         <Header />
         <Scoreboard currentScore={currentScore} total={allCards.length} highScore={highScore} />
       </div>
-      {console.log(touchedCards)}
-      <Display cards={Tarots.cards} touchCard={touchCard} />
+      <Display cards={currentCards} touchCard={touchCard} />
     </div>
   );
 }
